77f2e23f6d65f6fd40444d8c574f44c2356c1ea2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-08-28 19:02:48 +0900

    Fix build script

--- build/init.lua
+++ build/init.lua
@@ -5,10 +5,10 @@ local M = {}
 local Path = require("plenary.path")
 
 function M.build()
-  local output = vim.fn.system({ "cargo", "build", "--release", "--manifest-path", vim.g["ime-switch-win#cargo"] })
-  if vim.v.shell_error != 0 then
-    vim.fn.system({ "chmod", "+x", vim.g["ime-switch-win#bin"] })
-  end
+	local output = vim.fn.system({ "cargo", "build", "--release", "--manifest-path", vim.g["ime-switch-win#cargo"] })
+	if vim.v.shell_error ~= 0 then
+		vim.fn.system({ "chmod", "+x", vim.g["ime-switch-win#bin"] })
+	end
 end
 
 M.build()
