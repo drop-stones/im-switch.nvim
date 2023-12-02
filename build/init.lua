df7edd673c32b53d5d4f6205ba1dd8031ae0619b
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2023-12-02 11:54:58 +0900

    Make binary executable if cargo is not available

--- build/init.lua
+++ build/init.lua
@@ -5,9 +5,20 @@ local M = {}
 local Path = require("plenary.path")
 
 function M.build()
-	local output = vim.fn.system({ "cargo", "build", "--release", "--manifest-path", vim.g["ime-switch-win#cargo"] })
-	if vim.v.shell_error ~= 0 then
+	-- Make `bin/ime-switch-win.exe` executable
+	local add_executable_mode = function()
 		vim.fn.system({ "chmod", "+x", vim.g["ime-switch-win#bin"] })
+	end
+
+	if vim.fn.executable("cargo") == 1 then
+		vim.fn.system({ "cargo", "build", "--release", "--manifest-path", vim.g["ime-switch-win#cargo"] })
+		-- if build is failed, use `bin/ime-switch-win.exe` instead
+		if vim.v.shell_error ~= 0 then
+			add_executable_mode()
+		end
+	else
+		-- if cargo is not available, use `bin/ime-switch-win.exe` instead
+		add_executable_mode()
 	end
 end
 
