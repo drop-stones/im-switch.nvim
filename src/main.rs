0f3bdf4dd00d5dd094cf9513ea1aec7d67063f6b
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-01-17 22:21:53 +0900

    chore(src): formatter

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,29 @@ use windows::Win32::{
     UI::{Input::Ime::*, WindowsAndMessaging::*},
 };
 
-const IMC_GETOPENSTATUS: WPARAM = WPARAM { 0: 5 };
-const IMC_SETOPENSTATUS: WPARAM = WPARAM { 0: 6 };
+const IMC_GETOPENSTATUS: WPARAM = WPARAM(5);
+const IMC_SETOPENSTATUS: WPARAM = WPARAM(6);
 
 unsafe fn get_ime() -> HWND {
     let hwnd: HWND = GetForegroundWindow();
-    assert_ne!(hwnd, HWND { 0: 0 }, "Error: GetForegroundWindow failed");
+    assert_ne!(hwnd, HWND(0), "Error: GetForegroundWindow failed");
     let ime: HWND = ImmGetDefaultIMEWnd(hwnd);
-    assert_ne!(ime, HWND { 0: 0 }, "Error: ImmGetDefaultIMEWnd failed");
+    assert_ne!(ime, HWND(0), "Error: ImmGetDefaultIMEWnd failed");
     ime
 }
 
 unsafe fn get_ime_status() -> bool {
     let ime = get_ime();
-    match SendMessageA(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, LPARAM { 0: 0 }) {
-        LRESULT { 0: 0 } => false,
-        _ => true,
-    }
+    !matches!(
+        SendMessageA(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, LPARAM(0)),
+        LRESULT { 0: 0 }
+    )
 }
 
 unsafe fn set_ime_status(stat: bool) {
     let stat: LPARAM = match stat {
-        false => LPARAM { 0: 0 },
-        true => LPARAM { 0: 1 },
+        false => LPARAM(0),
+        true => LPARAM(1),
     };
     let ime = get_ime();
     SendMessageA(ime, WM_IME_CONTROL, IMC_SETOPENSTATUS, stat);
@@ -52,3 +52,4 @@ fn main() {
         }
     }
 }
+
