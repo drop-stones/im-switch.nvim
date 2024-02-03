0198a59ecdff0bdbe520ef45c83bae22e141447a
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-02-03 23:07:45 +0900

    feat(src): split files

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,16 @@
 use std::env;
-use windows::Win32::{
-    Foundation::*,
-    UI::{Input::Ime::*, WindowsAndMessaging::*},
-};
 
-const IMC_GETOPENSTATUS: WPARAM = WPARAM(5);
-const IMC_SETOPENSTATUS: WPARAM = WPARAM(6);
-
-unsafe fn get_ime() -> HWND {
-    let hwnd: HWND = GetForegroundWindow();
-    assert_ne!(hwnd, HWND(0), "Error: GetForegroundWindow failed");
-    let ime: HWND = ImmGetDefaultIMEWnd(hwnd);
-    assert_ne!(ime, HWND(0), "Error: ImmGetDefaultIMEWnd failed");
-    ime
-}
-
-unsafe fn get_ime_status() -> bool {
-    let ime = get_ime();
-    !matches!(
-        SendMessageA(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, LPARAM(0)),
-        LRESULT { 0: 0 }
-    )
-}
-
-unsafe fn set_ime_status(stat: bool) {
-    let stat: LPARAM = match stat {
-        false => LPARAM(0),
-        true => LPARAM(1),
-    };
-    let ime = get_ime();
-    SendMessageA(ime, WM_IME_CONTROL, IMC_SETOPENSTATUS, stat);
-}
+#[cfg(target_os = "windows")]
+mod windows;
+#[cfg(target_os = "windows")]
+use windows as ime;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let stat: bool;
     if args.len() >= 2 {
-        stat = match &*args[1] {
-            "on" | "open" | "1" => true,
-            "off" | "close" | "0" => false,
-            _ => panic!("Error: Invalid argument"),
-        };
-        unsafe {
-            set_ime_status(stat);
-        }
+        unsafe { ime::set_input_method(&args[1]) }
     } else {
-        unsafe {
-            stat = get_ime_status();
-            println!("{}", stat);
-        }
+        unsafe { println!("{}", ime::get_input_method()) }
     }
 }
-
---
+++ src/windows.rs
@@ -0,0 +1,33 @@
+use windows::Win32::{
+    Foundation::*,
+    UI::{Input::Ime::*, WindowsAndMessaging::*},
+};
+
+const IMC_GETOPENSTATUS: WPARAM = WPARAM(5);
+const IMC_SETOPENSTATUS: WPARAM = WPARAM(6);
+
+unsafe fn get_ime() -> HWND {
+    let hwnd: HWND = GetForegroundWindow();
+    assert_ne!(hwnd, HWND(0), "Error: GetForegroundWindow failed");
+    let ime: HWND = ImmGetDefaultIMEWnd(hwnd);
+    assert_ne!(ime, HWND(0), "Error: ImmGetDefaultIMEWnd failed");
+    ime
+}
+
+pub unsafe fn get_input_method() -> bool {
+    let ime = get_ime();
+    !matches!(
+        SendMessageA(ime, WM_IME_CONTROL, IMC_GETOPENSTATUS, LPARAM(0)),
+        LRESULT { 0: 0 }
+    )
+}
+
+pub unsafe fn set_input_method(locale: &str) {
+    let stat: LPARAM = match locale {
+        "on" | "open" | "1" => LPARAM(1),
+        "off" | "close" | "0" => LPARAM(0),
+        _ => panic!("Error: Invalid argument"),
+    };
+    let ime = get_ime();
+    SendMessageA(ime, WM_IME_CONTROL, IMC_SETOPENSTATUS, stat);
+}
