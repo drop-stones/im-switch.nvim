3a34fa5933ae4d151452c84c4402ca4c846261a2
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2024-02-11 16:47:11 +0900

    feat(src): add codes to change ime on macos

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ mod windows;
 #[cfg(target_os = "windows")]
 use windows::ime;
 
+#[cfg(target_os = "macos")]
+mod macos;
+#[cfg(target_os = "macos")]
+use macos::ime;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
