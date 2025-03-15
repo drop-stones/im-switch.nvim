467c000a3e342ec06b4cf34498de4532e24ccb25
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-15 20:57:31 +0900

    feat(utils): Refactoring utils.lua

--- lua/im-switch/utils.lua
+++ lua/im-switch/utils.lua
@@ -1,44 +1,53 @@
 local Path = require("plenary.path")
 
-local M = {}
+--==============================================================
+-- Local Functions
+--==============================================================
 
-function M.get_os()
-  if vim.fn.has("wsl") == 1 then
-    return "wsl"
-  elseif vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1 then
-    return "windows"
-  elseif vim.fn.has("mac") == 1 then
-    return "mac"
-  elseif vim.fn.has("linux") == 1 then
-    return "linux"
-  else
-    print("Unsupported OS")
-    os.exit(1)
+--- Detect the current operating system
+---@return string
+local function detect_os()
+  local os_map = {
+    wsl = "wsl",
+    win32 = "windows",
+    win64 = "windows",
+    mac = "mac",
+    linux = "linux",
+  }
+
+  for feature, os_name in pairs(os_map) do
+    if vim.fn.has(feature) == 1 then
+      return os_name
+    end
   end
+
+  vim.api.nvim_err_writeln("Unsupported OS")
+  error("Unsupported OS")
 end
 
-function M.need_cargo_build()
-  local os = M.get_os()
-  return (os == "mac") or (os == "windows")
-end
-
--- get root path
+--- Get the root path of the plugin using git
+---@return Path the root path
 local function get_plugin_root_path()
-  local utils_path = debug.getinfo(2, "S").source:sub(2)
-  if M.get_os() == "windows" then
-    -- need to replace path delimiters due to bugs of plenary
-    utils_path = string.gsub(utils_path, "/", "\\")
+  -- stylua: ignore
+  local path = debug.getinfo(1, "S").source:sub(2):match("(.*/)") -- path to im-switch.nvim/lua/im-switch/
+  local result = vim.system({ "git", "rev-parse", "--show-toplevel" }, { text = true, cwd = path }):wait()
+
+  if result.code ~= 0 then
+    vim.api.nvim_err_writeln("Git command failed in directory: " .. script_path())
+    vim.api.nvim_err_writeln("Error: " .. result.stderr)
+    error("Unable to determine plugin root path")
   end
-  return Path:new(utils_path):parent():parent():parent()
+
+  local root_path = vim.trim(result.stdout)
+
+  return Path:new(root_path)
 end
 
-function M.get_cargo_toml_path()
-  return get_plugin_root_path():joinpath("Cargo.toml"):absolute()
-end
-
--- get extension
+--- Get the executable file extension based on OS and whether it is prebuilt
+---@param is_prebuilt boolean whether the executable is prebuilt
+---@return string the appropriate file extension
 local function get_executable_extension(is_prebuilt)
-  local os = M.get_os()
+  local os = detect_os()
   if (os == "wsl") or (os == "windows") then
     return ".exe"
   elseif os == "mac" then
@@ -49,45 +58,83 @@ local function get_executable_extension(is_prebuilt)
   return ""
 end
 
+--==============================================================
+-- Public Functions
+--==============================================================
+
+local M = {}
+
+--- Detect the current operating system
+---@return string
+M.detect_os = detect_os
+
+--- Get the root path of the plugin using git
+---@return string the root path
+function M.get_plugin_root_path()
+  return get_plugin_root_path():absolute()
+end
+
+--- Check if cargo build is required based on the OS
+--- @return boolean
+function M.should_build_with_cargo()
+  local os = M.detect_os()
+  return (os == "mac") or (os == "windows")
+end
+
+--- Get the built executable path in the release directory
+---@return Path the absolute path to the built executable
 function M.get_built_executable_path()
   return get_plugin_root_path():joinpath("target/release/im-switch" .. get_executable_extension(false))
 end
 
-function M.get_executable_path()
-  local executable_path = M.get_built_executable_path()
-  local prebuilt_executable_path = get_plugin_root_path():joinpath("bin/im-switch" .. get_executable_extension(true))
-  if executable_path:exists() then
-    return executable_path:absolute()
-  elseif M.get_os() == "wsl" then -- FIXME: Workaround for WSL users
-    -- check whether the executable exists in %TEMP%
-    local temp_windows_path = vim.system({ "cmd.exe", "/c", "echo %TEMP%" }, { text = true }):wait()
-    local temp_wsl_path = vim.system({ "wslpath", "-au", temp_windows_path.stdout }):wait().stdout:gsub("%s+", "")
-    local executable_path_in_temp = temp_wsl_path .. "/im-switch" .. get_executable_extension(true)
-    local path = Path:new(executable_path_in_temp)
-    if not path:exists() then
-      -- copy executable to %TEMP% directory to speed up
-      vim.system({ "cp", prebuilt_executable_path:absolute(), temp_wsl_path }):wait()
-    end
-    return executable_path_in_temp
-  else
-    return prebuilt_executable_path:absolute()
-  end
+--- Get the prebuilt executable path in bin directory
+---@return string the absolute path to the prebuilt executable
+function M.get_prebuilt_executable_path()
+  return get_plugin_root_path():joinpath("bin/im-switch" .. get_executable_extension(true)):absolute()
 end
 
-function M.concat(list)
-  if type(list) == "table" then
-    local len = #list
-    if len == 0 then
-      return ""
+--- Get the appropriate executable path, checking for prebuilt versions or workarounds for WSL
+---@return string the absolute path to the executable
+function M.get_executable_path()
+  local executable_path = M.get_built_executable_path()
+  local prebuilt_executable_path = M.get_prebuilt_executable_path()
+
+  if executable_path:exists() then
+    return executable_path:absolute()
+  end
+
+  if M.detect_os() == "wsl" then -- FIXME: Workaround for WSL users
+    -- Get windows %TEMP% path and convert to WSL path
+    local temp_windows_path = vim.trim(vim.system({ "cmd.exe", "/c", "echo %TEMP%" }, { text = true }):wait().stdout)
+    local temp_wsl_path = vim.trim(vim.system({ "wslpath", "-au", temp_windows_path }, { text = true }):wait().stdout)
+
+    local executable_path_in_temp = temp_wsl_path .. "/im-switch" .. get_executable_extension(true)
+    local path = Path:new(executable_path_in_temp)
+
+    if not path:exists() then
+      -- Copy the prebuilt executable to the %TEMP% directory to speed up execution
+      local result = vim.system({ "cp", prebuilt_executable_path, temp_wsl_path }):wait()
+      if result.code ~= 0 then
+        error("Failed to copy prebuilt executable to %TEMP%: " .. result.stderr)
+      end
     end
 
-    local str = list[1]
-    for i = 2, len do
-      str = str .. " " .. list[i]
-    end
-    return str
-  else
+    return executable_path_in_temp
+  end
+
+  return prebuilt_executable_path
+end
+
+--- Concatenate a list of strings with spaces
+---@param list table|string input list or a single string
+---@return string the concatenated string
+function M.concat(list)
+  if type(list) == "string" then
     return list
+  elseif type(list) == "table" then
+    return table.concat(list, " ")
+  else
+    error("concat() expected a table or string, but got " .. type(list))
   end
 end
 
