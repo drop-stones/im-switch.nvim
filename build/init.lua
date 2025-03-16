eaafcf7c480217c1cbe6a125da4e5322b42843f5
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-16 17:26:59 +0900

    fix(build): Unload im-switch modules during installation

--- build/init.lua
+++ build/init.lua
@@ -1,4 +1,10 @@
--- Run cargo in installation
+-- Unload im-switch modules to load new files
+for module_name, _ in pairs(package.loaded) do
+  if module_name:match("^im%-switch") then
+    package.loaded[module_name] = nil
+  end
+end
+
 local utils = require("im-switch.utils")
 
 -- Skip build if cargo is not installed
