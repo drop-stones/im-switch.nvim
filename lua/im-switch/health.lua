e90bd6c722d1d4ff465cf41ee5a0c6a320ef5a21
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-16 15:39:54 +0900

    feat(health): Add checks to health.lua

--- lua/im-switch/health.lua
+++ lua/im-switch/health.lua
@@ -1,27 +1,75 @@
-local ok = vim.health.report_ok
-local warn = vim.health.report_warn
-local error = vim.health.report_error
-
+local opts = require("im-switch").opts
 local utils = require("im-switch.utils")
 
-local M = {}
 
-function M.check()
-  vim.health.start("im-switch.nvim")
+--- Reports plugin status based on OS-specific options
+local function check_os_options()
+  local os = utils.detect_os()
+  local platform_opts = opts[os]
+  if os == "wsl" then
+    platform_opts = opts.windows
+  end
 
-  if utils.need_cargo_build() then
-    if vim.fn.executable("cargo") == 1 then
-      ok("Cargo installed")
+  if not platform_opts then
+    vim.health.error(os .. " options are missing")
+    return
+  end
 
-      if utils.get_built_executable_path():exists() then
-        ok("Executable built correctly")
+  if platform_opts.enabled then
+    vim.health.ok("Plugin is enabled")
+    if os == "mac" or os == "linux" then
+      if type(platform_opts.default_im) == "string" then
+        vim.health.ok("default_im is " .. platform_opts.default_im)
       else
-        error("Executable built incorrectly")
+        vim.health.error("default_im is not configured")
       end
+    end
+    if os == "linux" then
+      for _, key in ipairs({ "obtain_im_command", "set_im_command" }) do
+        if type(platform_opts[key]) == "table" then
+          vim.health.ok(key .. " is " .. '"' .. utils.concat(platform_opts[key]) .. '"')
+        elseif type(platform_opts[key]) == "string" then
+          vim.health.ok(key .. " is " .. '"' .. platform_opts[key] .. '"')
+        else
+          vim.health.error(key .. " is not configured")
+        end
+      end
+    end
+  else
+    vim.health.ok("Plugin is disabled")
+  end
+end
+
+--- Check the availability of the im-switch binary
+local function check_binary()
+  if utils.should_build_with_cargo() and utils.executable("cargo") then
+
+    if utils.get_built_executable_path():exists() then
+      vim.health.ok("im-switch is built correctly")
     else
-      warn("Cargo not installed")
+      vim.health.error("im-switch is not built correctly")
+    end
+  else
+    local os, arch = utils.detect_os(), jit.arch
+    if ((os == "windows" or os == "wsl") and arch == "x64") or (os == "mac" and arch == "arm64") then
+      vim.health.ok("Prebuilt binary is used: " .. utils.get_prebuilt_executable_path())
+    else
+      vim.health.error("Prebuilt binary is not supported on this OS/architecture")
     end
   end
 end
 
-return M
+return {
+  check = function()
+    vim.health.start("im-switch.nvim")
+
+
+    if not opts then
+      vim.health.error("Plugin options are missing!")
+      return
+    end
+
+    check_os_options()
+    check_binary()
+  end,
+}
