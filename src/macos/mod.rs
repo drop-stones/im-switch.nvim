66697543369e9533f7301363829183487ce08152
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-17 20:17:32 +0900

    chore(rust): Split main.rs to windows/macos files

--- src/macos/mod.rs
+++ src/macos/mod.rs
@@ -1,2 +1,31 @@
-#[cfg(target_os = "macos")]
-pub mod im;
+mod args;
+mod im;
+
+use args::Args;
+use clap::{CommandFactory, Parser};
+use im::*;
+
+pub fn run() {
+    let args: Args = Args::parse();
+
+    if let Some(im) = args.set.as_deref() {
+        unsafe { set_input_method(im) };
+    } else if args.get {
+        unsafe { println!("{}", get_input_method()) };
+    } else {
+        let mut cmd = Args::command();
+        cmd.print_help().unwrap();
+    }
+}
+
+// Tests
+#[cfg(test)]
+mod test {
+    use super::*;
+    use clap::CommandFactory;
+
+    #[test]
+    fn test_args() {
+        Args::command().debug_assert()
+    }
+}
--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -1,2 +1,33 @@
-#[cfg(target_os = "windows")]
-pub mod im;
+mod args;
+mod im;
+
+use args::Args;
+use clap::{CommandFactory, Parser};
+use im::*;
+
+pub fn run() {
+    let args: Args = Args::parse();
+
+    if args.activate {
+        unsafe { activate_im() };
+    } else if args.inactivate {
+        unsafe { inactivate_im() };
+    } else if args.get {
+        unsafe { println!("{}", get_input_method()) };
+    } else {
+        let mut cmd = Args::command();
+        cmd.print_help().unwrap();
+    }
+}
+
+// Tests
+#[cfg(test)]
+mod test {
+    use super::*;
+    use clap::CommandFactory;
+
+    #[test]
+    fn test_args() {
+        Args::command().debug_assert()
+    }
+}
