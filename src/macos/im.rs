089bfb53905e11ba9f3288de8d4d6c2cd2b177d4
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-03-19 22:51:20 +0900

    feat(macos): Add validation checks for the given input method

--- src/macos/im.rs
+++ src/macos/im.rs
@@ -20,6 +20,32 @@ pub fn create_input_context() -> Result<Retained<NSTextInputContext>, Box<dyn Er
   Ok(input_context)
 }
 
+// Returns a list of available input methods
+pub fn get_available_input_methods() -> Result<Vec<&'static str>, Box<dyn Error>> {
+  let input_context: Retained<NSTextInputContext> = create_input_context()?;
+  let input_sources_ns: Vec<Retained<NSString>> = unsafe { input_context.keyboardInputSources() }
+    .ok_or_else(|| "Failed to get keyboard input sources".to_string())?
+    .to_vec();
+
+  // Convert the input sources to a vector of &str
+  let input_sources: Vec<&str> = input_sources_ns
+    .into_iter()
+    .map(|s| {
+      unsafe { CStr::from_ptr(s.UTF8String()) }
+        .to_str()
+        .map_err(|e| e.to_string())
+    })
+    .collect::<Result<Vec<&str>, String>>()?;
+
+  Ok(input_sources)
+}
+
+// Checks if a specific input method is available
+pub fn is_input_method_available(input_method: &str) -> Result<bool, Box<dyn Error>> {
+  let available_input_methods = get_available_input_methods()?;
+  Ok(available_input_methods.contains(&input_method))
+}
+
 pub fn get_input_method() -> Result<&'static str, Box<dyn Error>> {
   let input_context: Retained<NSTextInputContext> = create_input_context()?;
   let input_method: *const c_char = input_context
@@ -33,6 +59,19 @@ pub fn get_input_method() -> Result<&'static str, Box<dyn Error>> {
 }
 
 pub fn set_input_method(locale: &str) -> Result<(), Box<dyn Error>> {
+  // Check if the specified input method is available
+  if !is_input_method_available(input_method)? {
+    return Err(Box::from(format!(
+      "Input method not available for input method: {}",
+      input_method
+    )));
+  }
+
+  // If the current input method is already the desired one, do nothing and return success.
+  if get_input_method()? == input_method {
+    return Ok(());
+  }
+
   let input_context: Retained<NSTextInputContext> = create_input_context()?;
   let locale: Retained<NSTextInputSourceIdentifier> = NSString::from_str(locale);
   let locale: Option<&NSTextInputSourceIdentifier> = Some(locale.deref());
