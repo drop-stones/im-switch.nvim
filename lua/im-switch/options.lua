41f3b903383e0477436ddc813fa6b4e047331eb6
drop-stones <s2h6i8z1u7k8u7@gmail.com>
2025-06-06 19:26:46 +0900

    feat(types): Add types.lua for PluginOptions type definition

--- lua/im-switch/options.lua
+++ lua/im-switch/options.lua
@@ -1,30 +1,5 @@
 local os_utils = require("im-switch.utils.os")
 
---- Windows settings
----@class WindowsSettings
----@field enabled boolean
-
---- macOS settings
----@class MacosSettings
----@field enabled boolean
----@field default_im string
-
---- Linux settings
----@class LinuxSettings
----@field enabled boolean
----@field default_im string
----@field get_im_command string[]
----@field set_im_command string[]
-
---- Plugin options
----@class PluginOptions
----@field default_im_events string[]
----@field save_im_state_events string[]
----@field restore_im_events string[]
----@field windows WindowsSettings
----@field macos MacosSettings
----@field linux LinuxSettings
-
 --- Default plugin options
 ---@type PluginOptions
 local default_opts = {
